ea6eb0f3f764ef109fd3aa3f9493da4d76c9792f
lr94 <lucarobbiano@gmail.com>
2018-09-22 14:34:15 +0200

    Use an ARGB buffer instead of RGBA (and minor fixes)

--- png_bitmap.cpp
+++ png_bitmap.cpp
@@ -1,83 +1,101 @@
-#include <stdio.h>
+#include <cstdio>
 #include <png.h>
 #include "png_bitmap.hpp"
 
 png_bitmap::png_bitmap(int width, int height)
 {
-	this->width = width;
-	this->height = height;
+    this->width = width;
+    this->height = height;
 
-	this->data = new uint32_t[height * width];
+    this->data = new uint32_t[height * width];
 }
 
 png_bitmap::~png_bitmap()
 {
-	delete[] this->data;
+    delete[] this->data;
 }
 
 int png_bitmap::get_width()
 {
-	return this->width;
+    return this->width;
 }
 
 int png_bitmap::get_height()
 {
-	return this->height;
+    return this->height;
 }
 
 void png_bitmap::set_pixel(int x, int y, uint8_t r, uint8_t g, uint8_t b, uint8_t a)
 {
-	int i = y * this->width + x;
-	this->data[i] = (r << 24) | (g << 16) | (b << 8) | a;
+    int i = y * this->width + x;
+    this->data[i] = (a << 24) | (r << 16) | (g << 8) | b;
+}
+
+void png_bitmap::set_pixel(int x, int y, struct pixel color)
+{
+    this->set_pixel(x, y, color.r, color.g, color.b, color.a);
+}
+
+struct pixel png_bitmap::get_pixel(int x, int y)
+{
+    int i = y * this->width + x;
+
+    struct pixel color;
+    color.a = (uint8_t)((this->data[i] >> 24) & 0xff);
+    color.r = (uint8_t)((this->data[i] >> 16) & 0xff);
+    color.g = (uint8_t)((this->data[i] >>  8) & 0xff);
+    color.b = (uint8_t)(this->data[i] & 0xff);
+
+    return color;
 }
 
 int png_bitmap::save(std::string filename)
 {
-	FILE *fp = fopen(filename.c_str(), "wb");
-	if(fp == nullptr)
-		return -1;
+    FILE *fp = fopen(filename.c_str(), "wb");
+    if(fp == nullptr)
+        return -1;
 
-	png_structp png_ptr;
-	png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-	if(png_ptr == nullptr)
-	{
-		fclose(fp);
-		return -1;
-	}
+    png_structp png_ptr;
+    png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
+    if(png_ptr == nullptr)
+    {
+        fclose(fp);
+        return -1;
+    }
 
-	png_infop info_ptr;
-	info_ptr = png_create_info_struct(png_ptr);
-	if(info_ptr == nullptr)
-	{
-		fclose(fp);
-		return -1;
-	}
+    png_infop info_ptr;
+    info_ptr = png_create_info_struct(png_ptr);
+    if(info_ptr == nullptr)
+    {
+        fclose(fp);
+        return -1;
+    }
 
-	png_init_io(png_ptr, fp);
+    png_init_io(png_ptr, fp);
 
-	png_set_IHDR(png_ptr, info_ptr, this->width, this->height, 8, PNG_COLOR_TYPE_RGBA, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
+    png_set_IHDR(png_ptr, info_ptr, (uint32_t)this->width, (uint32_t)this->height, 8, PNG_COLOR_TYPE_RGBA, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
 
-	png_write_info(png_ptr, info_ptr);
+    png_write_info(png_ptr, info_ptr);
 
-	uint8_t *row = new uint8_t[4 * this->width];
-	for(int y = 0; y < this->height; y++)
-	{
-		for(int x = 0; x < this->width; x++)
-		{
-			int i = y * this->width + x;
-			row[x * 4] = (this->data[i] >> 24) & 0xff;
-			row[x * 4 + 1] = (this->data[i] >> 16) & 0xff;
-			row[x * 4 + 2] = (this->data[i] >>  8) & 0xff;
-			row[x * 4 + 3] = this->data[i] & 0xff;
-		}
+    uint8_t *row = new uint8_t[4 * this->width]; // RGBA
+    for(int y = 0; y < this->height; y++)
+    {
+        for(int x = 0; x < this->width; x++)
+        {
+            int i = y * this->width + x;
+            row[x * 4] = (uint8_t)((this->data[i] >> 16) & 0xff);;
+            row[x * 4 + 1] = (uint8_t)((this->data[i] >> 8) & 0xff);
+            row[x * 4 + 2] = (uint8_t)(this->data[i] & 0xff);
+            row[x * 4 + 3] = (uint8_t)((this->data[i] >> 24) & 0xff);
+        }
 
-		png_write_row(png_ptr, row);
-	}
-	delete[] row;
+        png_write_row(png_ptr, row);
+    }
+    delete[] row;
 
-	png_write_end(png_ptr, NULL);
+    png_write_end(png_ptr, NULL);
 
-	fclose(fp);
+    fclose(fp);
 
-	return 0;
+    return 0;
 }
--- png_bitmap.hpp
+++ png_bitmap.hpp
@@ -4,12 +4,19 @@
 #include <cstdint>
 #include <string>
 
+struct pixel
+{
+	uint8_t r;
+	uint8_t g;
+	uint8_t b;
+	uint8_t a;
+};
+
 class png_bitmap
 {
 	private:
 		int width;
 		int height;
-		uint32_t *data;
 
 	public:
 		png_bitmap(int width, int height);
@@ -19,8 +26,12 @@ class png_bitmap
 		int get_height();
 
 		void set_pixel(int x, int y, uint8_t r, uint8_t g, uint8_t b, uint8_t a);
+		void set_pixel(int x, int y, struct pixel color);
+		struct pixel get_pixel(int x, int y);
 
 		int save(std::string filename);
+
+		uint32_t *data; // ARGB
 };
 
 #endif
